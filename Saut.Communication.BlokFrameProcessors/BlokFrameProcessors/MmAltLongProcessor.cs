49b38bd7d9aaddf8bb6c4d001c915c95bb29b9d5
Evgeniy Plyusnin <eninbox@gmail.com>
2014-09-05 15:24:08 +0600

    Добавлен проект с базовыми обработчиками сообщений. Написан обработчик сообщений MmAltLongFrame

---
+++ Saut.Communication.BlokFrameProcessors/BlokFrameProcessors/MmAltLongProcessor.cs
@@ -0,0 +1,28 @@
+﻿using BlokFrames;
+using Geographics;
+using Saut.StateModel.StateProperties;
+
+namespace Saut.Communication.BlokFrameProcessors
+{
+    /// <summary>Обработчик сообщения MM_ALT_LONG с GPS-данными</summary>
+    public class MmAltLongProcessor : MessageProcessorBase<MmAltLongFrame>
+    {
+        private readonly GpsPositionProperty _positionProperty;
+        private readonly GpsReliabilityProperty _reliabilityProperty;
+
+        public MmAltLongProcessor(GpsPositionProperty PositionProperty, GpsReliabilityProperty ReliabilityProperty)
+        {
+            _positionProperty = PositionProperty;
+            _reliabilityProperty = ReliabilityProperty;
+        }
+
+        /// <summary>Обрабатывает поступившее сообщение</summary>
+        /// <param name="Message">Сообщение</param>
+        public override void ProcessMessage(MmAltLongFrame Message)
+        {
+            var position = new EarthPoint(Message.Latitude, Message.Longitude);
+            _positionProperty.UpdateValue(position, Message.Time);
+            _reliabilityProperty.UpdateValue(Message.Reliable, Message.Time);
+        }
+    }
+}
---
+++ Saut.Communication.BlokFrameProcessors/Modules/BlokFrameProcessorsModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Practices.Unity;
+using Modules;
+using Modules.Dependencies;
+using Saut.Communication.BlokFrameProcessors;
+using Saut.StateModel;
+
+namespace Saut.Communication.Modules
+{
+    [DependOn(typeof (IStateProperty))]
+    [Provides(typeof (IMessageProcessor))]
+    public class BlokFrameProcessorsModule : IModule
+    {
+        /// <summary>Конфигурирует контейнер</summary>
+        /// <remarks>Здесь нужно зарегистрировать все типы, предоставляемые этим модулем наружу и используемые им самим</remarks>
+        /// <param name="Container">Конфигурируемый контейнер</param>
+        public void ConfigureContainer(IUnityContainer Container) { Container.RegisterType<IMessageProcessor, MmAltLongProcessor>("MmAltLongProcessor"); }
+
+        /// <summary>Инициализирует модуль</summary>
+        /// <remarks>Здесь нужно запустить всё, что нужно запустить, создать всё, что нужно создать.</remarks>
+        /// <param name="Container">Сконфигурированный контейнер приложения</param>
+        public void InitializeModule(IUnityContainer Container) { }
+    }
+}
