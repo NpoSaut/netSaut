234105dad5dc7926039de6437da7f2def2b9df8b
Evgeniy Plyusnin <eninbox@gmail.com>
2014-09-10 16:56:43 +0600

    Принято решение отказаться от StateModelService, все свойства просто будут передаваться в соответствующие модули через контейнер. Возможно, потом придётся придумать что-нибудь другое, но пока что только так.

--- Saut.StateModel/Modules/StateModelModule.cs
+++ Saut.StateModel/Modules/StateModelModule.cs
@@ -9,20 +9,13 @@ using Saut.StateModel.Journals;
 
 namespace Saut.StateModel.Modules
 {
-    [Provides(typeof (IStateModelService), typeof (IStateProperty))]
-    [DependOn(typeof (IStateProperty))]
     public class StateModelModule : IModule
     {
-        private IStateModelService _stateModelService;
-
         /// <summary>Конфигурирует контейнер.</summary>
         /// <remarks>Здесь нужно зарегистрировать все типы, предоставляемые этим модулем наружу и используемые им самим.</remarks>
         /// <param name="Container">Конфигурируемый контейнер.</param>
         public void ConfigureContainer(IUnityContainer Container)
         {
-            // Внешние сервисы
-            Container.RegisterType<IStateModelService, StateModelService>();
-
             // Интерполяторы
             Container.RegisterType<IWeightingTool<Double>, NumericWeightingTool>();
             Container.RegisterType<IInterpolator<Double>, LinearInterpolator<Double>>();
@@ -42,6 +35,6 @@ namespace Saut.StateModel.Modules
         /// <summary>Инициализирует модуль.</summary>
         /// <remarks>Здесь нужно запустить всё, что нужно запустить, создать всё, что нужно создать.</remarks>
         /// <param name="Container">Сконфигурированный контейнер приложения.</param>
-        public void InitializeModule(IUnityContainer Container) { _stateModelService = Container.Resolve<IStateModelService>(); }
+        public void InitializeModule(IUnityContainer Container) { }
     }
 }
