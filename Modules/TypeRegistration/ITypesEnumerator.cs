f6541c9aa240e34bbc0bdc0325e56911d7abc608
Evgeniy Plyusnin <eninbox@gmail.com>
2014-09-10 16:42:32 +0600

    В Modules добавлены ITypesEnumerator и IRegistrant, полезные для пакетной регистрации различных типов. А так же их реализации для поиска всех типов, реализующих интерфейс в сборке и регистрации синглетонов

---
+++ Modules/TypeRegistration/ITypesEnumerator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Modules.TypeRegistration
+{
+    public interface ITypesEnumerator
+    {
+        IEnumerable<Type> EnumerateTypes(); 
+    }
+}
