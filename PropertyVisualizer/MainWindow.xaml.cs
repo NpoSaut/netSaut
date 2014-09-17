82fc99fc894da8a0723b0f0d8337593fd5769525
Evgeniy Plyusnin <eninbox@gmail.com>
2014-09-17 18:22:51 +0600

    Закатал демку для показа графиков

---
+++ PropertyVisualizer/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using GraphVisualizing.GraphProviders;
+using GraphVisualizing.Projection;
+
+namespace PropertyVisualizer
+{
+    /// <summary>Логика взаимодействия для MainWindow.xaml</summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ((ScaleShiftProjector<Double, Double>)GraphView.Projector).ScaleX = 30;
+            ((ScaleShiftProjector<Double, Double>)GraphView.Projector).ScaleY = 30;
+            ((ScaleShiftProjector<Double, Double>)GraphView.Projector).X0 = 0;
+            ((ScaleShiftProjector<Double, Double>)GraphView.Projector).Y0 = 2;
+            GraphView.GraphProvider = new GP(Math.Sin, new Pen(new SolidColorBrush(Colors.Red), 3));
+        }
+
+        private void GraphView_OnLoaded(object Sender, RoutedEventArgs E)
+        {
+            GraphView.Refresh();
+        }
+    }
+}
